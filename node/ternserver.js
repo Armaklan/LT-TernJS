5f4b7a65f90ea0f095da4f1586106b7b4f7faeb5
Armaklan <lionel.zuber@armaklan.org>
2015-07-22 22:25:46 +0200

    Refactoring of TernJs Encapsulation. Delete unused variable. Extract some method. Reorganize global variable.

--- node/ternserver.js
+++ node/ternserver.js
@@ -1,21 +1,61 @@
+/* If true, add many trace */
+var DEBUG = true;
+
+/* External dependencies */
 var fs = require('fs'),
-    path = require('path'),
     tern = require('tern'),
-    os = require('os'),
-    module_dir = path.join(__dirname, '../node_modules/tern'),
-    plugin_dir = path.join(module_dir, 'plugin'),
-    defs_dir = path.join(module_dir, 'defs'),
-    maxIdleTime = 6e4 * 5, // Shut down after five minutes of inactivity
-    shutdown = setTimeout(doShutdown, maxIdleTime),
-    isWin = os.platform() === 'win32',
-    DEBUG = false,
-    INFO = 'INFO',
-    WARNING = 'WARNING',
-    ERROR = 'ERROR';
+    os = require('os');
+
+/* Timeout gestion. Shutdown is a shared state. */
+var maxIdleTime = 6e4 * 5, // Shut down after five minutes of inactivity
+    shutdown = setTimeout(doShutdown, maxIdleTime);
+
+/* Use to fix filesystem */
+var isWin = os.platform() === 'win32';
+
+var logLevel = {
+  INFO : 'INFO',
+  WARNING : 'WARNING',
+  ERROR : 'ERROR'
+};
+
+/* Contains TernJs instance */
+var server;
+
+/* Contains last receive message. Use to return it when unknown error is throw.
+  TODO - Delete shared state */
+var currentmsg;
+
+var asyncImportFiles = (function() {
+  var cachedFiles = [],
+      count = 0,
+      nextFile;
+  function next(server) {
+    nextFile = cachedFiles.pop();
+    if (nextFile) {
+      try {
+        server.addFile(nextFile);
+      } catch(e) {
+        _log(logLevel.ERROR, 'An error occured while loading file: ', {file: nextFile});
+        send(e);
+      }
+      count++;
+      setTimeout(function() { next(server);}, 0);
+      return;
+    }
+    _log(logLevel.INFO, 'Finished loading files', {time: new Date(), count: count });
+  }
+  return function(server, files) {
+    _log(logLevel.INFO, 'Start loading files: ', {startTime: new Date(), fileCount: files.length});
+    cachedFiles = cachedFiles.concat(files);
+    next(server);
+  };
+}());
+
 
 function doShutdown() {
   console.log('Was idle for ' + Math.floor(maxIdleTime / 6e4) + ' minutes. Shutting down.');
-  process.exit();
+  closeTernProcess();
 }
 
 function send(err, data, msg) {
@@ -45,9 +85,8 @@ function loadLibs(paths) {
   return paths.map(function(x) { return JSON.parse(fs.readFileSync(x)); });
 }
 
-
 function loadPlugins(paths) {
-  if (!paths) { _log(INFO, 'No plugins loaded'); }
+  if (!paths) { _log(logLevel.INFO, 'No plugins loaded'); }
   var plugins = {};
   (paths || []).forEach(function(x) {
     require(x.path);
@@ -56,15 +95,13 @@ function loadPlugins(paths) {
   return plugins;
 }
 
-
-var server;
 function getServer(msg) {
   if (server) { return server; }
-  _log(INFO, 'getServer(msg) : ', msg.command);
+  _log(logLevel.INFO, 'getServer(msg) : ', msg.command);
   if (msg.command !== 'init') {
     throw new Error('Server not started and on init message received');
   }
-  _log(INFO, 'Creating new tern server', msg);
+  _log(logLevel.INFO, 'Creating new tern server', msg);
   server = new tern.Server({
     async: true,
     defs: loadLibs(msg.data.payload.config.libs),
@@ -74,42 +111,21 @@ function getServer(msg) {
       if (!isWin && path && path[0] !== '/') {
         path = '/' + x;
       }
-      _log(INFO, "Attempting to load file", path);
+      _log(logLevel.INFO, "Attempting to load file", path);
       fs.readFile(path, {encoding: 'utf8'}, cb);
     }
   });
   return server;
 }
 
-var asyncImportFiles = (function() {
-  var cachedFiles = [],
-      count = 0,
-      nextFile;
-  function next(server) {
-    nextFile = cachedFiles.pop();
-    if (nextFile) {
-      try {
-        server.addFile(nextFile);
-      } catch(e) {
-        _log(ERROR, 'An error occured while loading file: ', {file: nextFile});
-        send(e);
-      }
-      count++;
-      setTimeout(function() { next(server);}, 0);
-      return;
-    }
-    _log(INFO, 'Finished loading files', {time: new Date(), count: count });
-  }
-  return function(server, files) {
-    _log(INFO, 'Start loading files: ', {startTime: new Date(), fileCount: files.length});
-    cachedFiles = cachedFiles.concat(files);
-    next(server);
-  };
-}());
+process.on('message', processMessage);
+process.on('SIGINT', closeTernProcess);
+process.on('SIGTERM', closeTernProcess);
+process.on('uncaughtException', function (err) {
+  send(err, {}, currentmsg);
+});
 
-
-var currentmsg;
-process.on('message', function(msg) {
+function processMessage(msg) {
   clearTimeout(shutdown);
   shutdown = setTimeout(doShutdown, maxIdleTime);
   currentmsg = msg;
@@ -118,28 +134,16 @@ process.on('message', function(msg) {
         data = msg.data || {};
     switch(data.type) {
       case 'request':
-        _log(INFO, 'Received message', data.payload);
-        srv.request(data.payload, function(e, out) {
-          _log(INFO, 'Sending message', out);
-          send(e, out, msg);
-        });
-        _log(INFO, 'Server files', srv.files.map(function(x) { return x.name; }));
+        processRequest(srv, msg, data);
         break;
       case 'addfiles':
-        asyncImportFiles(srv, data.payload);
-        send(null, {}, msg);
+        processAddFiles(srv, msg, data);
         break;
       case 'deletefiles':
-        data.payload.forEach(function(x) {
-          srv.delFile(x);
-        });
-        send(null, {}, msg);
+        processDeleteFiles(srv, msg, data);
         break;
       case 'init':
-        _log(INFO, 'Init server');
-        if (!data.payload.paths) { _log(WARNING, 'No files found for loading'); }
-        asyncImportFiles(srv, data.payload.paths || []);
-        send(null, {}, msg);
+        processInit(srv, msg, data);
         break;
     }
   } catch (e) {
@@ -148,10 +152,36 @@ process.on('message', function(msg) {
     send(e, {}, currentmsg);
     doShutdown();
   }
-});
+}
 
-process.on('SIGINT', function() { process.exit(); });
-process.on('SIGTERM', function() { process.exit(); });
-process.on('uncaughtException', function (err) {
-  send(err, {}, currentmsg);
-});
+function processRequest(srv, msg, data) {
+  _log(logLevel.INFO, 'Received message', data.payload);
+  srv.request(data.payload, function(err, out) {
+    _log(logLevel.INFO, 'Sending message', out);
+    send(err, out, msg);
+  });
+  _log(logLevel.INFO, 'Server files', srv.files.map(function(x) { return x.name; }));
+}
+
+function processAddFiles(srv, msg, data) {
+  asyncImportFiles(srv, data.payload);
+  send(null, {}, msg);
+}
+
+function processDeleteFiles(srv, msg, data) {
+  data.payload.forEach(function(file) {
+    srv.delFile(file);
+  });
+  send(null, {}, msg);
+}
+
+function processInit(srv, msg, data) {
+  _log(logLevel.INFO, 'Init server');
+  if (!data.payload.paths) { _log(logLevel.WARNING, 'No files found for loading'); }
+  asyncImportFiles(srv, data.payload.paths || []);
+  send(null, {}, msg);
+}
+
+function closeTernProcess() {
+  process.exit();
+}
